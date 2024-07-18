5247c79db5422c439cb7a8651484bc1c74768325
Chang <changsheng@sql.com.my>
2024-07-18 11:40:45 +0800

    feat: refactor endpoints

---
+++ endpoints.go
@@ -0,0 +1,60 @@
+package myinvois
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const (
+	apiPrefixV10 = "/api/v1.0"
+
+	// platform
+	loginEndpoint         = "/connect/token"
+	documentTypesEndpoint = "/documenttypes"
+
+	// einvoice
+	validateTaxpayerTinEndpoint  = "/taxpayer/validate"
+	submitDocumentsEndpoint      = "/documentsubmissions"
+	getDocumentsEndpoint         = "/documents"
+	updateDocumentStatusEndpoint = "/documents/state/%s/state"
+	getRecentDocumentsEndpoint   = "/documents/recent"
+
+	defaultGrantType = "client_credentials"
+	defaultScope     = "InvoicingAPI"
+)
+
+type platformEndpoints struct {
+	loginAsTaxpayer       *url.URL
+	loginAsIntermediaries *url.URL
+	getAllDocumentTypes   *url.URL
+	getDocumentType       *url.URL
+}
+
+var (
+	PlatformEndpoints = &platformEndpoints{
+		loginAsTaxpayer:       &url.URL{Path: loginEndpoint},
+		loginAsIntermediaries: &url.URL{Path: loginEndpoint},
+		getAllDocumentTypes:   &url.URL{Path: apiPrefixV10 + documentTypesEndpoint},
+		getDocumentType:       &url.URL{Path: apiPrefixV10 + documentTypesEndpoint},
+	}
+)
+
+type einvoiceEndpoints struct {
+	validateTaxpayerTIN *url.URL
+	submitDocuments     *url.URL
+	getDocuments        *url.URL
+	getRecentDocuments  *url.URL
+}
+
+var (
+	EinvoiceEndpoints = &einvoiceEndpoints{
+		validateTaxpayerTIN: &url.URL{Path: apiPrefixV10 + validateTaxpayerTinEndpoint},
+		submitDocuments:     &url.URL{Path: apiPrefixV10 + submitDocumentsEndpoint},
+		getDocuments:        &url.URL{Path: apiPrefixV10 + getDocumentsEndpoint},
+		getRecentDocuments:  &url.URL{Path: apiPrefixV10 + getRecentDocumentsEndpoint},
+	}
+)
+
+func (e *einvoiceEndpoints) updateDocumentStatus(uuid string) *url.URL {
+	return &url.URL{Path: apiPrefixV10 + fmt.Sprintf(updateDocumentStatusEndpoint, uuid)}
+}
